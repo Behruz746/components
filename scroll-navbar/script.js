c0346df9629031943e852379ad60f8306860dd07
Behruz <madaminovbehruz746@gmail.com>
2024-04-25 10:58:33 +0500

    added: scroll navbar component

---
+++ scroll-navbar/script.js
@@ -0,0 +1,33 @@
+const headerEl = document.querySelector(".header");
+const btnTopEl = document.querySelector("#scroll-top");
+
+let previousScrollPosition = window.pageYOffset;
+
+function getHeaderP() {
+  const currentScrollPosition = +window.pageYOffset.toFixed();
+
+  if (scrollY >= 50) {
+    headerEl.style.padding = `20px 0`;
+    btnTopEl.style.transform = `scale(1)`;
+  } else if (scrollY <= 50) {
+    headerEl.style.padding = `50px 0`;
+    btnTopEl.style.transform = `scale(0)`;
+  }
+
+  if (currentScrollPosition >= 200) {
+    if (currentScrollPosition > previousScrollPosition) {
+      headerEl.style.top = "-200px";
+    } else {
+      headerEl.style.top = "0";
+    }
+  }
+
+  previousScrollPosition = currentScrollPosition;
+}
+
+function scrollTop() {
+  document.documentElement.scrollTop = 0;
+}
+
+window.addEventListener("scroll", getHeaderP);
+btnTopEl.addEventListener("click", scrollTop);
